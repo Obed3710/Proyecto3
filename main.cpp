61927faf2ebe618efd2efbd790c15ed0032607fb
Obed <casasolaobed92@gmail.com>
2021-07-14 19:39:51 -0600

    Subiendo Proyecto3

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	int n;
+	
+	
+	cout<<" inigrese un numero:"<<endl;
+	cin>>n;
+	cout<<endl;
+	
+	for(int i=1;i<=10;i++){
+		cout<<n<<" x "<<i<<" = "<<n*i<<endl;
+		
+	}
+	
+		
+	
+	return 0;
+}
